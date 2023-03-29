c5d25e2e21d1f6af96ac9c68823087d48f47ec93
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-03-29 13:11:51 -0600

    boton para elegir el idioma

--- telefono_milan/telefono_milan.ino
+++ telefono_milan/telefono_milan.ino
@@ -16,7 +16,18 @@ int idoma = 0;
 const byte interruptPin = 3; // Pin del interruptor de límite
 volatile bool triggered = false; // Variable booleana que indica si se ha activado la interrupción
 
+// Definir los pines del botón
+const int BUTTON_1_PIN = 5;
+const int BUTTON_2_PIN = 7;
 
+// Definir el tiempo de espera antes de la acción de fallback (en milisegundos)
+const unsigned long TIMEOUT = 61000;
+
+// Definir las variables para el estado anterior del botón y el tiempo de la última pulsación
+bool lastButtonState_1 = LOW;
+bool lastButtonState_2 = LOW;
+unsigned long lastButtonPressTime = 0;
+int lastButton = 0;
 
 // ================== DECLARACION PARA EL TIMER. =================
 // Definir el tiempo en milisegundos para el temporizador de un minuto
@@ -32,7 +43,11 @@ void printDetail(uint8_t type, int value);
 void setup() {
   // put your setup code here, to run once:
   pinMode(interruptPin, INPUT_PULLUP); // Configuración del pin como entrada con resistencia pull-up interna
-  attachInterrupt(digitalPinToInterrupt(interruptPin), interruptFunction, CHANGE); // Configuración de la interrupción en el cambio de estado del pin
+  pinMode(BUTTON_1_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_2_PIN, INPUT_PULLUP);
+
+  // Configuración de la interrupción en el cambio de estado del pin
+  attachInterrupt(digitalPinToInterrupt(interruptPin), interruptFunction, CHANGE); 
 
   mySoftwareSerial.begin(9600);
   Serial.begin(9600);
@@ -50,17 +65,11 @@ void setup() {
     }
   Serial.println(F("DFPlayer Mini online."));
   myDFPlayer.volume(15);
-  
 }
-
 
 void loop() {
   controlState();
   States();
-  // myDFPlayer.play(1);
-  // delay(5000);
-  // Serial.println(triggered);
-
 }
 
 void interruptFunction() {
@@ -80,6 +89,11 @@ void interruptFunction() {
   valorAnterior = valorActual;
 }
 
+
+
+
+
+
 void controlState(){
 
   if (millis() - lastActionTime >= minute) {
@@ -90,6 +104,36 @@ void controlState(){
 
   if(triggered == 1 && currentState == state1 ){
     currentState = state2;
+  }
+
+  // Leer el estado actual de los botones
+  bool buttonState_1 = digitalRead(BUTTON_1_PIN);
+  bool buttonState_2 = digitalRead(BUTTON_2_PIN);
+
+  // Verificar si alguno de los botones se ha presionado
+  if (buttonState_1 != lastButtonState_1 || buttonState_2 != lastButtonState_2) {
+    // Actualizar el tiempo de la última pulsación de botón
+    lastButtonPressTime = millis();
+
+    // Verificar qué botón se ha presionado
+    if (buttonState_1 == LOW) {
+      // Realizar la acción para el botón 1
+      lastButton = 1;
+
+    } else if (buttonState_2 == LOW){
+      // Realizar la acción para el botón 2
+      lastButton = 2;
+    }
+    // Actualizar el estado anterior del botón
+    lastButtonState_1 = buttonState_1;
+    lastButtonState_2 = buttonState_2;
+  } else {
+    // Verificar si ha pasado el tiempo de espera para la acción de fallback
+    if (millis() - lastButtonPressTime >= TIMEOUT) {
+      // Realizar la acción de fallback
+      lastButton = 3;
+
+    }
   }
 
 }
@@ -124,21 +168,29 @@ void stateOne(){
 
 void stateTwo(){
   Serial.println("estado dos");
-  if(idoma == 0){
+  int randomNumber = random(1, 5);
+  if(lastButton == 1){
     // ESP
-    myDFPlayer.play(2);
-    delay(9000);
+    myDFPlayer.play(randomNumber);
+    delay(40000);
+    myDFPlayer.pause();
     currentState = nullState;
-  }else if(idoma == 1){
+  }else if(lastButton == 2){
     // ING
-
-  }else{
+    myDFPlayer.play(randomNumber+5);
+    delay(42000);
+    myDFPlayer.pause();
+    currentState = nullState;
+  }else if(lastButton == 3){
     // ITL
-
+    myDFPlayer.play(randomNumber+10);
+    delay(45000);
+    myDFPlayer.pause();
+    currentState = nullState;
   }  
 }
 
 void nullState_n(){
-  Serial.println("Limbo");
+  Serial.println(lastButton);
 }
 
